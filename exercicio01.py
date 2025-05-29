935db1b803475814cca74dbbb4b5875f688f9dd0
Frez-Luiz <luiz.frez.santos@escola.pr.gov.br>
2025-05-29 14:37:47 +0000

    terminei exercicio 1, metade do 2, 3.

--- exercicio01.py
+++ exercicio01.py
@@ -4,3 +4,12 @@
 # Crie um programa que receba o número de avaliações de cada curso e exiba qual teve mais.
 # Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve empate.
 
+curso01 = int(input("Quantas avaliações o cursos01 teve?"))
+curso02 = int(input("Quantas avaliações o cursos02 teve?"))
+
+if curso01 > curso02:
+    print ("Curso01 teve mais avaliações")
+elif curso01 < curso02:
+    print ("Curso02 teve mais avaliações")
+else:
+    print("Teve um empate nas avaliações")
--- exercicio02.py
+++ exercicio02.py
@@ -2,4 +2,19 @@
 # para finalizar três tarefas: X, Y e Z.
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
+# Soma ok, só não funciona o negativo.
 
+tarefaX = int(input("Digite o tempo da tarefa X."))
+tarefaY = int(input("Digite o tempo da tarefa Y."))
+tarefaZ = int(input("Digite o tempo da tarefa Z."))
+              
+soma = tarefaX + tarefaY + tarefaZ
+if tarefaX  < 0:
+    print("ERRO! Seu tempo foi negativo.")
+elif tarefaY  < 0:
+    print("ERRO! Seu tempo foi negativo.")
+elif tarefaZ  < 0:
+    print("ERRO! Seu tempo foi negativo.")
+else:
+    print("Seu tempo foi:" , soma)
+    
