311f4e3158491bc233fca69f1407b872a5cc5cb8
Frez-Luiz <luiz.frez.santos@escola.pr.gov.br>
2025-06-12 12:20:15 +0000

    fiz mais um pela m,etade

--- exercicio16.py
+++ exercicio16.py
@@ -3,3 +3,14 @@
 # Salários até R$ 2000,00: +15%
 # De R$ 2000,01 a R$ 5000,00: +10%
 # Acima de R$ 5000,00: +5%
+
+
+salario = float(input("Digite seu salário."))
+
+reajuste1 = salario*0.15
+reajuste2 = salario*0.10
+reajuste3 = salario*0.05
+
+
+if salario <= 2000:
+    print("Você ganhou um reajuste de + 15%.")
