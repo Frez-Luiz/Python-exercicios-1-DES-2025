393776e9d05a8d7e4c5b629a3ea0f00f807190a8
Frez-Luiz <luiz.frez.santos@escola.pr.gov.br>
2025-06-15 22:27:31 +0000

    só falta o 13 e o 16 pra finalizar tudo.

--- exercicio17.py
+++ exercicio17.py
@@ -1,11 +1,15 @@
 # Peça ao usuário uma temperatura em Celsius e converta para Fahrenheit.
 # Fórmula: F = C × 1.8 + 32
 
-temperatura = int(input("Digite uma temperatura em Celsius."))
+temperatura = int(input("Digite uma temperatura em Celsius:"))
 
 fahrenheit = temperatura * 1.8 + 32
 
-print()"A temperatura foi": , fahrenheit)
+if fahrenheit:
+    print("A temperatura em fahrenheit foi:" ,  fahrenheit)
+
+
+#finalizado
 
 
 
