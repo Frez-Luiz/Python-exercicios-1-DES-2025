e43ec9153518154952fa899a4726ae9f6deeac3e
Frez-Luiz <luiz.frez.santos@escola.pr.gov.br>
2025-06-16 14:06:01 +0000

    metade do 16

--- exercicio16.py
+++ exercicio16.py
@@ -13,4 +13,11 @@ reajuste3 = salario*0.05
 
 
 if salario <= 2000:
-    print("Você ganhou um reajuste de + 15%.")
+    print("Você ganhou um reajuste de + 15%, que foi de +" , reajuste1)
+elif  2000.01 <= salario <= 5000:
+    print("Você ganhou um reajuste de + 10%, que foi de +" , reajuste2)
+elif salario > 5000:
+    print("Você ganhou um reajuste de + 5%, que foi de +" , reajuste3)
+
+
+#finalizado pela metade
