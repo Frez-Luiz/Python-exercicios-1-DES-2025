f15c8b211efd039cb6f9463384cc98f774618813
Frez-Luiz <luiz.frez.santos@escola.pr.gov.br>
2025-06-17 12:20:26 +0000

    novo exercicio

---
+++ carta.py
@@ -0,0 +1,7 @@
+email = input("Digite um e-mail que deseja mandar uma mensagem:")
+mensagem = input("Digite sua mensagem:")
+remetente = input("Digite seu nome:")
+
+print("\n--- Sua carta ---")
+print(f"Olá {email}, \n\n{mensagem}\n\nE-mail enviado por, \n{remetente} no dia 17/06/2025.")
+
