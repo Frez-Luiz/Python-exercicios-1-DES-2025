daaa68091672fed499bde54b5ff68d5f9c18cc6e
Frez-Luiz <luiz.frez.santos@escola.pr.gov.br>
2025-06-26 12:09:37 +0000

    novos exercicios

---
+++ exemploTexto.py
@@ -0,0 +1,10 @@
+
+print ("OLá, mundo!")
+
+nome = "Luiz Felipe"
+sobrenome = "Santos"
+
+print ("Nome completo: " + nome + " " + sobrenome)
+
+idade = 14
+print(f"{nome} {sobrenome} tem {idade} anos.")
---
+++ m_texto.py
@@ -0,0 +1,10 @@
+
+texto = "Quem anda na frente, nunca anda atrás!"
+
+print(texto.upper()) 
+print(texto.lower()) 
+
+print(len(texto))
+
+novo_texto = texto.replace("Incrivel", "poderoso")
+print(novo_texto) 
